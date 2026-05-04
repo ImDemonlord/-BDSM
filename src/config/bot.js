5e70dd6400eeb3cdd24881fd2571af4b90404f8b
ImDemonlord <81275970+ImDemonlord@users.noreply.github.com>
2026-05-04 17:47:05 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Gooning To Russian Girls ❤️",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
